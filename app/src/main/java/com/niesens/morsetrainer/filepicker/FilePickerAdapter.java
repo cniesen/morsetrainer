21e495894a18f2b611b256a4d979f0d9ccad4a8f
Claus Niesen <cniesen@users.noreply.github.com>
2019-09-03 22:11:34 -0500

    Added GPL v3 license to source code. Closes #27.

--- app/src/main/java/com/niesens/morsetrainer/filepicker/FilePickerAdapter.java
+++ app/src/main/java/com/niesens/morsetrainer/filepicker/FilePickerAdapter.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2019 Claus Niesen
+ *
+ *  This file is part of Claus' Morse Trainer.
+ *
+ *  Claus' Morse Trainer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Claus' Morse Trainer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Claus' Morse Trainer.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.niesens.morsetrainer.filepicker;
 
 import android.app.Activity;
